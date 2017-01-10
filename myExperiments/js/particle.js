43d1bad295843f06d627fb30fbb4092f84ed8f78
abhishekg785 <abhishekg785@gmail.com>
2017-01-10 17:31:10 +0530

    gravity working properly and the canvas clear rect bug removed

--- myExperiments/js/particle.js
+++ myExperiments/js/particle.js
@@ -31,58 +31,114 @@
         this.width = canvas.width;
         this.ctx = canvas.getContext('2d');
         this.radius = 10;
-        this.x = 30;
-        this.y = 30;
-        this.draw();
+        this.pos = new Vec(30, 30);
+        this.mouse = new Vec(0, 0);
+        this.mouseDown = false;
+        this.draggedEntity = null;
+
+        // parameters for adding simulation to the universe
+        this.gravity = new Vec(0, 9.8);
+
+        // functions related to the canvas
+
+        var _this = this;
+        _this.canvas.oncontextmenu = function() {
+            e.preventDefault();
+        }
+
+        _this.canvas.onmouseup = function(e) {
+            _this.mouseDown = false;
+            _this.draggedEntity = null;
+        }
+
+        _this.canvas.onmousedown = function(e) {
+            _this.mouseDown = true;
+            _this.draggedEntity = Globals.particle;
+        }
+
+        _this.canvas.onmousemove = function(e) {
+            var rect = _this.canvas.getBoundingClientRect();
+            _this.mouse.x = e.clientX - rect.left;
+            _this.mouse.y = e.clientY - rect.top;
+        }
     }
 
     Particle.prototype.draw = function() {
-        this.ctx.clearRect(0, 0, $Objects.canvas.height, $Objects.canvas.width);
+        this.ctx.clearRect(0, 0, $Objects.canvas.width, $Objects.canvas.height);
         this.ctx.beginPath();
-        this.ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+        this.ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2*Math.PI);
         this.ctx.fillStyle = "red";
         this.ctx.fill();
     }
 
     /*
-        preventing the particle to not pass through the canvas bounds
-        x = get the x pos of particle
-        y = get the y pos of the particle
-        conditions :
-        x > 0 && x < this.width
-        y < this.height
+     preventing the particle to not pass through the canvas bounds
+     x = get the x pos of particle
+     y = get the y pos of the particle
+     conditions :
+     x > 0 && x < this.width
+     y < this.height
 
      */
     Particle.prototype.bounds = function() {
 
+        console.log('in the bound function');
         // fixing y position
-        if(Globals.particle.y > this.height - 1) {
-            Globals.particle.y = this.height - 2;
+        if(Globals.particle.pos.y > this.height - 30) {
+            Globals.particle.pos.y = this.height - 30;
         }
 
-        if(Globals.particle.x < 0) {
-            Globals.particle.x = 0;
+        if(Globals.particle.pos.x < 0) {
+            Globals.particle.pos.x = 0;
         }
 
-        if(Globals.particle.x > this.width - 1) {
-            Globals.particle.x = this.width - 1;
+        if(Globals.particle.pos.x > this.width - 1) {
+            console.log('vklfnvklnfv');
+            Globals.particle.pos.x = this.width - 1;
         }
 
     }
 
-    function applyGravity() {
-        Globals.particle.y += 0.7;
-        console.log(Globals.particle.x);
-        Globals.particle.draw();
-        Globals.particle.bounds();
-        w.requestAnimationFrame(applyGravity);
+    Particle.prototype.makeDrag = function() {
+        if(this.draggedEntity) {
+            this.pos.mutableSet(this.mouse);
+        }
+    }
+
+    // responsible for the pull of the particle in the downward direction
+    // handle the dragging of the particle too.
+    Particle.prototype.applyGravity = function() {
+        this.pos.y += this.gravity.y;
+        this.draw();
+        this.bounds();
+        this.makeDrag();
+    }
+
+    // vector class for implementing vector  ( some math shit :P )
+    function Vec(x, y) {
+        this.x = x || 0;
+        this.y = y || 0;
+        return this;
+    }
+
+    Vec.prototype.set = function(pos) {
+
+    }
+
+    Vec.prototype.mutableSet = function(pos) {
+        this.x = pos.x;
+        this.y = pos.y;
     }
 
     $Objects = {};
     $w.on('load', function() {
         $Objects.canvas = $('#canvas')[0];
         Globals.particle = new Particle($Objects.canvas);
-        // applyGravity();
+        function animate() {
+            Globals.particle.applyGravity();
+            w.requestAnimationFrame(animate);
+        }
+        animate();
     });
 
 
